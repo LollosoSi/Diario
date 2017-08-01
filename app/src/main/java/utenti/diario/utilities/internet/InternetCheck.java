1b711177f81b7275dabe3591446ab13aa1e9f169
LollosoSi <lollosositv@gmail.com>
2017-08-01 18:46:10 +0200

    Modification

---
+++ app/src/main/java/utenti/diario/utilities/internet/InternetCheck.java
@@ -0,0 +1,9 @@
+package utenti.diario.utilities.internet;
+
+/**
+ * Created by SosiForWork on 01/08/2017.
+ */
+
+public interface InternetCheck {
+    void OnConnectionChecked(boolean isOnline);
+}

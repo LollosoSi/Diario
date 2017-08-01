7239bebcece7c826a157feca4c5f8110f98b1974
LollosoSi <lollosositv@gmail.com>
2017-08-01 18:49:43 +0200

    Modification

--- app/src/main/java/utenti/diario/utilities/internet/InternetManager.java
+++ app/src/main/java/utenti/diario/utilities/internet/InternetManager.java
@@ -1,8 +1,39 @@
 package utenti.diario.utilities.internet;
 
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 /**
  * Created by SosiForWork on 01/08/2017.
  */
 
-public class InternetManager {
+public class InternetManager extends AsyncTask<InternetCheck,Void,Void> {
+
+        @Override
+        protected Void doInBackground(InternetCheck... params) {
+
+            InternetCheck cl=params[0];
+
+            try {
+                int timeoutMs = 1500;
+                Socket sock = new Socket();
+                SocketAddress sockaddr = new InetSocketAddress("8.8.8.8", 53);
+
+                sock.connect(sockaddr, timeoutMs);
+                sock.close();
+                cl.OnConnectionChecked(true);
+
+            } catch(IOException e) {
+                cl.OnConnectionChecked(false);
+
+            }
+
+            return null;
+        }
+
+
 }

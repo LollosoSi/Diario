14c7351fce369b67f0940e4c5de6bea4911cdc50
LollosoSi <lollosositv@gmail.com>
2017-08-01 19:19:57 +0200

    Modification

---
+++ app/src/main/java/utenti/diario/activities/home/Home.java
@@ -0,0 +1,15 @@
+package utenti.diario.activities.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import utenti.diario.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/utenti/diario/activities/login/Login.java
@@ -0,0 +1,15 @@
+package utenti.diario.activities.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import utenti.diario.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

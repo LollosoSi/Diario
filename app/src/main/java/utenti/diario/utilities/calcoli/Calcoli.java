d6d4adb050357c5a1776a8856d77923cdb503e05
LollosoSi <lollosositv@gmail.com>
2017-08-01 18:04:17 +0200

    Modification

---
+++ app/src/main/java/utenti/diario/utilities/calcoli/Calcoli.java
@@ -0,0 +1,8 @@
+package utenti.diario.utilities.calcoli;
+
+/**
+ * Created by SosiForWork on 01/08/2017.
+ */
+
+public class Calcoli {
+}
---
+++ app/src/main/java/utenti/diario/utilities/internet/InternetManager.java
@@ -0,0 +1,8 @@
+package utenti.diario.utilities.internet;
+
+/**
+ * Created by SosiForWork on 01/08/2017.
+ */
+
+public class InternetManager {
+}
